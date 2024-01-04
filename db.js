938eebf8703a3eae322d2774c95dbf06497b4c8b
MohammadMatin-EbrahimiToodeshki <92645903+MohammadMatin-EbrahimiToodeshki@users.noreply.github.com>
2024-01-04 16:29:22 +0000

    Building an Express Application with MongoDB Integration

---
+++ db.js
@@ -0,0 +1,53 @@
+
+// const { MongoClient, ServerApiVersion } = require('mongodb');
+// const uri = "mongodb+srv://ebrahimitoodeshki:Matin1381@cluster0.geqaesh.mongodb.net/?retryWrites=true&w=majority";
+
+// // Create a MongoClient with a MongoClientOptions object to set the Stable API version
+// const client = new MongoClient(uri, {
+//   serverApi: {
+//     version: ServerApiVersion.v1,
+//     strict: true,
+//     deprecationErrors: true,
+//   }
+// });
+
+// async function run() {
+//   try {
+//     // Connect the client to the server	(optional starting in v4.7)
+//     await client.connect();
+//     // Send a ping to confirm a successful connection
+//     await client.db("admin").command({ ping: 1 });
+//     console.log("Pinged your deployment. You successfully connected to MongoDB!");
+//   } finally {
+//     // Ensures that the client will close when you finish/error
+//     await client.close();
+//   }
+// }
+// run().catch(console.dir);
+
+
+
+const { MongoClient } = require('mongodb');
+
+// Replace 'your-mongodb-uri' with your actual MongoDB connection string
+const uri = 'mongodb+srv://ebrahimitoodeshki:Matin1381@cluster0.geqaesh.mongodb.net/?retryWrites=true&w=majority';
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+async function connectToMongoDB() {
+  try {
+    await client.connect();
+    console.log('Connected to MongoDB');
+    return client.db('Cluster0'); // Replace 'your-database-name' with your actual database name
+  } catch (error) {
+    console.error('Error connecting to MongoDB:', error);
+    throw error;
+  }
+}
+
+function closeMongoDBConnection() {
+  client.close();
+  console.log('MongoDB connection closed');
+}
+
+module.exports = { connectToMongoDB, closeMongoDBConnection };
